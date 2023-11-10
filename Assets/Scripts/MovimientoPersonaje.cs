29c95a538e963e29c08d1f32c48c3c6f5a26b7ee
Pablo98L <lopezpablo.m98@gmail.com>
2023-11-10 17:15:03 -0300

    Ajustes ambos personajes

--- Assets/Scripts/MovimientoPersonaje.cs
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -9,12 +9,14 @@ public class MovimientoPersonaje : MonoBehaviour
 
 
     private bool subiendo = false;
+    private bool subirDos = false;
     private float step; // Movimiento 
     private int objActual; // objeto actual en el arreglo
 
     void Start()
     {
         objActual = 0;
+        transform.localScale = new Vector3(1, 1, 1);
         step = velocidad * Time.deltaTime;
     }
 
@@ -23,16 +25,21 @@ public class MovimientoPersonaje : MonoBehaviour
         
         if (subiendo)
             {
-            objActual = 1;
+            SubirEscalera();
+            /*objActual = 1;
             transform.position = Vector3.MoveTowards(transform.position, objetoColision[objActual].position, step);
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-1, 1, 1);*/
+
+        } else if (subirDos)
+        {
+            SubirEscaleraDos();
 
         }
             else
             {
             
             transform.position = Vector3.MoveTowards(transform.position, objetoColision[objActual].position, step);
-            transform.localScale = new Vector3(1, 1, 1);
+            
         }
         
     }
@@ -42,7 +49,13 @@ public class MovimientoPersonaje : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Escalera01"))
         {
-            SubirEscalera();
+            subiendo = true;
+            //SubirEscalera();
+        }
+        else if (collision.gameObject.CompareTag("Escalera02"))
+        {
+            subirDos = true;
+            //SubirEscaleraDos();
         }
     }
 
@@ -52,8 +65,24 @@ public class MovimientoPersonaje : MonoBehaviour
         Vector3 nuevaPosicion = transform.position + new Vector3(0, alturaSalto, 0);
         transform.position = nuevaPosicion;
 
-        // Cambiar la dirección a la izquierda
-        subiendo = true;        
+      
+        subiendo = false;
+
+        //--objeto actual y flip der al sprite
+        objActual = 1;
+        transform.localScale = new Vector3(-1, 1, 1);
+    }
+    void SubirEscaleraDos()
+    {
+        // Subir a una altura determinada
+        Vector3 nuevaPosicion = transform.position + new Vector3(0, alturaSalto, 0);
+        transform.position = nuevaPosicion;
+
+        subirDos = false;
+
+        //---objeto actual y flip izq al sprite
+        objActual = 2;
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }
 
