b87b33702c33a8d646e3dd2f24615d25ace5feb3
Pablo98L <lopezpablo.m98@gmail.com>
2023-11-12 23:06:39 -0300

    pjMaria loop
    Caminata en loop

--- Assets/Scripts/MovimientoMaria.cs
+++ Assets/Scripts/MovimientoMaria.cs
@@ -11,44 +11,103 @@ public class MovimientoMaria : MonoBehaviour
     private int objActual = 0;
     private float step;
 
+    private bool primerEstado=true;
+    private bool segundoEstado;
+    private bool regresar=false;
+    private bool subir = false;
+    private bool subirDos = false;
+
     void Start()
     {
-        objActual = 0;
-        // Voltear sprite hacia la derecha
-        transform.localScale = new Vector3(-1, 1, 1);
-        step = velocidad * Time.deltaTime;
+        
+            
+            objActual = 0;
+            // Voltear sprite hacia la derecha
+            transform.localScale = new Vector3(-1, 1, 1);
+            step = velocidad * Time.deltaTime;
+       
+        
+        
+     
+
+        
+        segundoEstado = false;
     }
 
     void Update()
     {
-        if (bajando)
-        {
-            BajarEscalera();
-        }
-        else if (bajarDos)
-        {
-            BajarEscaleraDos();
-        }
-        else
-        {
-            // Movimiento horizontal
+        if (primerEstado) {
             transform.position = Vector3.MoveTowards(transform.position, objetoColision[objActual].position, step);
-            //transform.localScale = new Vector3((objActual == 0) ? -1 : 1, 1, 1);
+            if (bajando)
+            {
+                BajarEscalera();
+            }
+            else if (bajarDos)
+            {
+                BajarEscaleraDos();
+            }
+            else
+            {
+                // Movimiento horizontal
+                //transform.position = Vector3.MoveTowards(transform.position, objetoColision[objActual].position, step);
+                //transform.localScale = new Vector3((objActual == 0) ? -1 : 1, 1, 1);
+            }
         }
+
+        if (segundoEstado)
+        {
+            
+            if (subir)
+            {
+                subirEscalera01();
+            }
+            else if (subirDos)
+            {
+                subirEscalera02();
+            }
+
+        }
+
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision Detected");
 
-        if (collision.gameObject.CompareTag("Silla"))
+        if (primerEstado)
         {
-            bajando = true;
+            if (collision.gameObject.CompareTag("Silla"))
+            {
+                bajando = true;
+            }
+            else if (collision.gameObject.CompareTag("Lavarropa"))
+            {
+                bajarDos = true;
+            }
+            else if (collision.gameObject.CompareTag("ColiMaria03"))
+            {
+                //segundoEstado = true;
+                regreso();
+            }
         }
-        else if (collision.gameObject.CompareTag("Lavarropa"))
+        if (segundoEstado)
         {
-            bajarDos = true;
+            if (collision.gameObject.CompareTag("Escalera01"))
+            {
+                subir = true;
+            }
+            if (collision.gameObject.CompareTag("Escalera02"))
+            {
+                subirDos = true;
+            }
+            if (collision.gameObject.CompareTag("ultimaPrincipio"))
+            {
+                reiniciar();
+            }
         }
+       
+        
     }
 
     void BajarEscalera()
@@ -76,6 +135,53 @@ public class MovimientoMaria : MonoBehaviour
         bajarDos = false;
         // Voltear sprite hacia la derecha
         transform.localScale = new Vector3(-1, 1, 1);
+    }
+
+    void subirEscalera01()
+    {
+       
+        // Cambiar la dirección
+        objActual = 4;
+        subir = false;
+
+        Vector3 nuevaPosicion = transform.position + new Vector3(0, 290, 0);
+        transform.position = nuevaPosicion;
+
+        // Voltear sprite hacia la derecha
+        transform.localScale = new Vector3(-1, 1, 1);
+
+    }
+
+    void subirEscalera02()
+    {
+        // Cambiar la dirección
+        objActual = 5;
+        subirDos = false;
+
+        Vector3 nuevaPosicion = transform.position + new Vector3(0, 290, 0);
+        transform.position = nuevaPosicion;
+
+        // Voltear sprite hacia la derecha
+        transform.localScale = new Vector3(1, 1, 1);
+
+    }
+
+    void regreso()
+    {
+        // Movimiento horizontal
+        transform.position = Vector3.MoveTowards(transform.position, objetoColision[objActual].position, step);
+
+        // Cambiar la dirección
+        objActual = 3;
+        //bajarDos = false;
+        // Voltear sprite hacia la derecha
+        transform.localScale = new Vector3(1, 1, 1);
+        segundoEstado = true;
+    }
+
+    void reiniciar()
+    {
+        Start();
     }
 }
 
